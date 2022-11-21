ad14bf1eb156d44bf58c6fefb76c0246ac4c5600
maulie5 <83541036+maulie5@users.noreply.github.com>
2022-11-21 11:18:06 +0100

    WIP 0.1.1
    Minor bug ficks

--- Minigame/Items/Item.java
+++ Minigame/Items/Item.java
@@ -17,11 +17,32 @@ public class Item {
     protected String m_sItemType;
     protected int m_nItemLevel;
     protected int m_nItemID;
-    //protected String m_s
+    private double m_dDamageFactor;
+    private double m_dDamage;
+    private double m_dRange;
+    private double m_dRangeFactor;
+    private int m_nNutritionalValue;
+    private String m_sCategory;    
+    private String m_sRarity
+
+    public Item(String sName, String sType, String sCategory, int nLevel, String sRarity, double dDamage, double dRange, double dDamageFactor, double dRangeFactor, int nNutritionalValue){
+     
+        m_sItemType = sType;
+        m_sItemName = sName;
+        m_nItemLevel = nLevel;
+        m_sCategory = sCategory;
+        m_dDamageFactor = dDamageFactor;
+        m_dDamage = dDamage;
+        m_dRangeFactor = dRangeFactor;
+        m_dRange = dRange;
+        m_nNutritionalValue = nNutritionalValue;
+        m_sRarity = sRarity;
+    }
 
     public void LevelUp() {
         m_nItemLevel++;
     }
+
 
 
 }
