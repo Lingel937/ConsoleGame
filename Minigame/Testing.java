1ca0663587c8e7c89de60a2f12869ee82619a106
Valentin Lingelbach <valentin.lingelbach@gmail.com>
2022-12-13 14:32:04 +0100

    WIP_0.1.1.7  //generate Item rarity function implemented

--- Minigame/Testing.java
+++ Minigame/Testing.java
@@ -2,6 +2,13 @@ import Characters.enemies.Enemy;
 
 public class Testing {
     public static void main(String[] args) {
-        Enemy.generateEnemy();
+
+
+
+       System.out.println(Basic.generateRarity());
+
+
+
+
     }
 }
