39388a78fb2d115c1a8998b9df8a27e6253128c5
Jamal Sabel <83541036+maulie5@users.noreply.github.com>
2022-12-13 15:00:24 +0100

    Update Item.java

--- Minigame/Items/Item.java
+++ Minigame/Items/Item.java
@@ -11,19 +11,19 @@
 package Items;
 public class Item {
     //membervariables for an item
-    public String m_sItemName;
-    public String m_sItemType;
-    public int m_nItemLevel;
-    public int m_nItemID;
-    public String m_sCategory;    
-    public String m_sRarity;
-    public int m_nNumberOfItems;
+    private String m_sItemName;
+    private String m_sItemType;
+    private int m_nItemLevel;
+    private int m_nItemID;
+    private String m_sCategory;    
+    private String m_sRarity;
+    private int m_nNumberOfItems;
 
     //membervariables for weapons:
-    public double m_dDamageFactor;
-    public double m_dDamage;
-    public double m_dRange;
-    public double m_dRangeFactor;
+    private double m_dDamageFactor;
+    private double m_dDamage;
+    private double m_dRange;
+    private double m_dRangeFactor;
     
     //membervariables for the food
     public int m_nNutritionalValue;
@@ -58,8 +58,64 @@ public class Item {
         m_nNumberOfItems =nNumberOfItems;
 
     }
+    
+    //list of getters for an item object
 
     public void LevelUp() {
         m_nItemLevel++;
+    }
+
+    public int getID(){
+        return m_nItemID;
+    }
+
+    public String getName(){
+        return m_sItemName;
+    }
+
+    public String getType(){
+        return m_sItemType;
+    }
+
+    public int getLvl(){
+        return m_nItemLevel;
+    }
+    
+    public String getCategory(){
+        return m_sCategory;
+    }
+
+    public double getDamage(){
+        return m_dDamage;
+    }
+
+    public double getDamageFactor(){
+        return m_dDamageFactor;
+    }
+
+    public double getRange() {
+        return m_dRange;
+    }
+
+    public double getRangeFactor() {
+        return m_dRangeFactor;
+    }
+
+
+    //list of setters for item objects
+    public void setName(String sName){
+        m_sItemName = sName;
+    }
+
+    public void setLvl(int nLevel){
+        m_nItemLevel = nLevel;
+    }
+    
+    public void setDamage(double dDamage){
+        m_dDamage = dDamage;
+    }
+    
+    public void setDamage(double dDamageFactor){
+        m_dDamageFactor = dDamageFactor;
     }
 }
