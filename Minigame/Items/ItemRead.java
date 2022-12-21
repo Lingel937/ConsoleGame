873de6deefb678d24a6d3720b859279ba659fddd
maulie5 <jamal.sabel@proton.me>
2022-12-21 09:10:41 +0100

    Update ItemRead.java

--- Minigame/Items/ItemRead.java
+++ Minigame/Items/ItemRead.java
@@ -1,9 +1,12 @@
 package Items;
+
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
+
 public class ItemRead {
 
+<<<<<<< Updated upstream
 	public static int getNumberOfLinesOfItemFile(){
 		try{
 		File itemReadFile = new File("C:\\Users\\Valentin\\Documents\\Schule\\E phase\\Praktische Informatik\\Minigame\\Items\\Items.txt");
@@ -12,46 +15,55 @@ public class ItemRead {
 		
 		while(itemRead.hasNextLine()){
 			singleLine.add(itemRead.nextLine());
-		}
-
-		int int_numberOfLines = singleLine.size();
-		itemRead.close();
-		singleLine.clear();
-		return int_numberOfLines;
-	}
-	catch(FileNotFoundException e){
-		System.out.println("You need an 'Items' file.");
-		e.printStackTrace();
-		return 1;
-	}
-
-	}
-	public static String[] getItemProperties(int int_line){
-		try{
-			String[] itemProperties;
-			File itemReadFile = new File("\"C:\\Users\\Valentin\\Documents\\Schule\\E phase\\Praktische Informatik\\Minigame\\Minigame\\Items\\Itemz.txt\"");
+=======
+	public static int getNumberOfLinesOfItemFile() {
+		try {
+			File itemReadFile = new File("./Itemz.txt");
 			Scanner itemRead = new Scanner(itemReadFile);
 			ArrayList<String> singleLine = new ArrayList<String>();
-			
-			while(itemRead.hasNextLine()){
+
+			while (itemRead.hasNextLine()) {
+				singleLine.add(itemRead.nextLine());
+			}
+
+			int int_numberOfLines = singleLine.size();
+			itemRead.close();
+			singleLine.clear();
+			return int_numberOfLines;
+		} catch (FileNotFoundException e) {
+			System.out.println("You need an 'Items' file.");
+			e.printStackTrace();
+			return 1;
+>>>>>>> Stashed changes
+		}
+
+	}
+
+	public static String[] getItemProperties(int int_line) {
+		try {
+			String[] itemProperties;
+<<<<<<< Updated upstream
+			File itemReadFile = new File("\"C:\\Users\\Valentin\\Documents\\Schule\\E phase\\Praktische Informatik\\Minigame\\Minigame\\Items\\Itemz.txt\"");
+=======
+			File itemReadFile = new File("./Itemz.txt");
+>>>>>>> Stashed changes
+			Scanner itemRead = new Scanner(itemReadFile);
+			ArrayList<String> singleLine = new ArrayList<String>();
+
+			while (itemRead.hasNextLine()) {
 				singleLine.add(itemRead.nextLine());
 			}
 			itemRead.close();
 			itemProperties = singleLine.get(int_line).split(";");
 			singleLine.clear();
 			return itemProperties;
-		}
-		catch(FileNotFoundException e){
-			String[] error = {"Error"};
+		} catch (FileNotFoundException e) {
+			String[] error = { "Error" };
 			System.out.println("You need an 'Items' file.");
 			e.printStackTrace();
 			return error;
 		}
-		
-		
-		
 
 	}
-
 
 }
