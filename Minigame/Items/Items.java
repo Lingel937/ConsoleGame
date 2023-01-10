332de1392d3905488187991ff9cbf19b25caef52
Lo118 <42409590+Lo118@users.noreply.github.com>
2023-01-10 14:02:12 +0100

    Update Items.java

--- Minigame/Items/Items.java
+++ Minigame/Items/Items.java
@@ -23,42 +23,46 @@ public class Items {
     public static void itemslol(){
 
 
+        // (1)(Name)(Type)(category)(level)(damage)(damagefactor)(nutrition)(Count)(Protection)
         //resources
-        wood = new Item(1,"wood","Resource","",0, 0,0,0,0, 0);
-        hay = new Item(1,"hay","Resource","",0,0,0,0,0,0);
+        wood = new Item(1,"wood","Resource","",0, 0,0,0,1, 0);
+        hay = new Item(1,"hay","Resource","",0,0,0,0,1,0);
 
         //food
-        raspberry = new Item(1, "Raspberry", "Food","",1,0,0,10,0,0);
-
-
+        raspberry = new Item(1, "raspberry", "Food","",1,0,0,10,1,0);
+        bread = new Item(118,"bread","Food","",1,0,0,50,1,0);
+        Jamal = new Item(118,"Jamal","Food","",1,0,0,1000,1,0);
 
         //weapons
-        Heugabel = new Item(1, "Heugabel","Weapon", "",1,10,0.10,0,0,0);
-        sword = new Item(1, "sword","Weapon", "",1,25,0.25,0,0,0);
+        pitchfork = new Item(1, "pitchfork","Weapon", "",1,10,0.10,0,1,0);
+        sword = new Item(1, "sword","Weapon", "",1,25,0.25,0,1,0);
+        club = new Item(118, "club","Weapon","",1,8,0.1,0,1,0);
+        baton = new Item(118,"baton","Weapon","",1,12,0.2,0,1,0);
+        prisonShank = new Item(118,"prison shank","Weapon","",1,15,0.05,0,1,0);
 
         //armor
 
             //helmets
-            LeatherHelmet = new Item(1, "LeatherHelmet","Armor","Helmet",0,0,0,0,0,3);
+            leatherHelmet = new Item(1, "leather helmet","Armor","Helmet",0,0,0,0,1,3);
 
             //BodyArmor
-            IronChestplate =new Item(1, "IronChestplate","Armor","Boots",0,0,0,0,0,30);
-            ClothTunik = new Item(1, "ClothTunik","Armor","BodyArmor",0,0,0,0,0,6);
-            BulletproofVest = new Item(1, "BulletproofVest","Armor","BodyArmor",0,0,0,0,0,40);
+            ironChestplate =new Item(1, "iron chestplate","Armor","Boots",0,0,0,0,1,30);
+            clothTunik = new Item(1, "cloth tunik","Armor","BodyArmor",0,0,0,0,1,6);
+            bulletproofVest = new Item(1, "bullet proof vest","Armor","BodyArmor",0,0,0,0,1,40);
 
             //Pants
-            LeatherPants = new Item(1, "LeatherPants","Armor","Pants",0,0,0,0,0,13);
-            CargoPants = new Item(1, "CargoPants","Armor","Pants",0,0,0,0,0,10);
+            leatherPants = new Item(1, "leather pants","Armor","Pants",0,0,0,0,1,13);
+            cargoPants = new Item(1, "cargo pants","Armor","Pants",0,0,0,0,1,10);
 
             //boots
 
-            WoodenBoots = new Item(1, "WoodenBoots","Armor","Boots",0,0,0,0,0,3);
-            LeatherBoots = new Item(1, "LeatherBoots","Armor","Boots",0,0,0,0,0,5);
+            woodenBoots = new Item(1, "wooden boots","Armor","Boots",0,0,0,0,1,3);
+            leatherBoots = new Item(1, "leather boots","Armor","Boots",0,0,0,0,1,5);
 
             //
-            Barefoot = new Item(1,"none","Armor","Boots",0,0,0,0,0,4);
-            NoBodyarmor = new Item(1,"none","Armor","BodyArmor",0,0,0,0,0,20);
-            NoHat = new Item(1,"none","Armor","Helmet",0,0,0,0,0,6);
-            NoPants = new Item(1,"none","Armor","Pants",0,0,0,0,0,10);
+            barefoot = new Item(1,"none","Armor","Boots",0,0,0,0,1,4);
+            NoBodyarmor = new Item(1,"none","Armor","BodyArmor",0,0,0,0,1,20);
+            NoHat = new Item(1,"none","Armor","Helmet",0,0,0,0,1,6);
+            NoPants = new Item(1,"none","Armor","Pants",0,0,0,0,1,10);
     }
 }
