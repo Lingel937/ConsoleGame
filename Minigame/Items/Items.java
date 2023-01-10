6145fa10bf08345f9d107186657d7f9d46729139
maulie5 <jamal.sabel@proton.me>
2023-01-10 14:34:22 +0100

    Revert "Update Items.java"
    This reverts commit f1fbcf4ceb8d13a832e32135e4df10c73512b37b.

--- Minigame/Items/Items.java
+++ Minigame/Items/Items.java
@@ -5,26 +5,25 @@ public class Items {
     public static Item sword;
     public static Item raspberry;
     public static Item hay;
-    public static Item pitchfork;
+    public static Item pitchfok;
     public static Item bread;
-    public static Item jamal;
+    public static Item Jamal;
     public static Item club;
     public static Item baton;
     public static Item prisonshank;
-    public static Item largestick;
-    public static Item stone;
-    public static Item arrow;
-    public static Item axe;
-    
-    public static Item mace;
-    public static Item brassknuckels;
-    public static Item sythe;
-    public static Item book;
-    public static Item chainwhip;
-    public static Item ropewhip;
-    public static Item humanleg;
-    public static Item humanarm;
-    public static Item humanskullmace;
+    public static Item ;
+    public static Item ;
+    public static Item ;
+    public static Item ;
+    public static Item ;
+    public static Item ;
+    public static Item ;
+    public static Item ;
+    public static Item ;
+    public static Item ;
+    public static Item ;
+    public static Item ;
+    public static Item ;
 
     public static Item leatherhelmet;
     public static Item leatherboots;
@@ -46,12 +45,11 @@ public class Items {
         //resources
         wood = new Item(1,"wood","Resource","",0, 0,0,0,1, 0);
         hay = new Item(1,"hay","Resource","",0,0,0,0,1,0);
-        stone = new Item(118,"stone","Rescource","",0,0,0,0,1,0);
 
         //food
         raspberry = new Item(1, "raspberry", "Food","",1,0,0,10,1,0);
         bread = new Item(118,"bread","Food","",1,0,0,50,1,0);
-        jamal = new Item(118,"Jamal","Food","",1,0,0,1000,1,0);
+        Jamal = new Item(118,"Jamal","Food","",1,0,0,1000,1,0);
 
         //weapons
         pitchfork = new Item(1, "pitchfork","Weapon", "",1,10,0.10,0,1,0);
@@ -59,10 +57,6 @@ public class Items {
         club = new Item(118, "club","Weapon","",1,8,0.1,0,1,0);
         baton = new Item(118,"baton","Weapon","",1,12,0.2,0,1,0);
         prisonshank = new Item(118,"prison shank","Weapon","",1,15,0.05,0,1,0);
-        largestick = new Item(118,"large stick","Weapon","",1,5,0.1,0,1,0);
-        arrow = new Item(118,"arrow","Weapon","",1,7,0.05,0,1,0);
-        axe = new Item(118,"axe","Weapon","",1,27,0.25,0,1,0);
-        
 
         //armor
 
