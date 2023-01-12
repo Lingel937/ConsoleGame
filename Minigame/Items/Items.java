29117f07602e2b7c8fd6745f30a3efb82fa1106c
Lo118 <42409590+Lo118@users.noreply.github.com>
2023-01-12 10:34:08 +0100

    Update Items.java

--- Minigame/Items/Items.java
+++ Minigame/Items/Items.java
@@ -14,12 +14,14 @@ public class Items {
     
     //weapons
     
+        //>5dmg
+        public static Item book;
+        
         //>10dmg
         public static Item club;
         public static Item largestick;
         public static Item arrow;
         public static Item brassknuckles;
-        public static Item book;
         public static Item humanleg;
         public static Item humanarm;
         public static Item humanskull;
@@ -80,24 +82,36 @@ public class Items {
         jamal = new Item(118,"Jamal","Food","",1,0,0,1000,1,0);
 
         //weapons
-        pitchfork = new Item(1, "pitchfork","Weapon", "",1,10,0.10,0,1,0);
-        sword = new Item(1, "sword","Weapon", "",1,25,0.25,0,1,0);
-        club = new Item(118, "club","Weapon","",1,8,0.1,0,1,0);
-        baton = new Item(118,"baton","Weapon","",1,12,0.2,0,1,0);
-        prisonshank = new Item(118,"prison shank","Weapon","",1,15,0.05,0,1,0);
-        largestick = new Item(118,"large stick","Weapon","",1,5,0.1,0,1,0);
-        arrow = new Item(118,"arrow","Weapon","",1,7,0.05,0,1,0);
-        axe = new Item(118,"axe","Weapon","",1,27,0.25,0,1,0);
-        mace = new Item(118,"mace","Weapon","",1,14,0.1,0,1,0);
-        brassknuckles = new Item(118,"brass knuckles","Weapon","",1,6,0.05,0,1,0);
-        sythe = new Item(118,"sythe","Weapon","",1,20,0.2,0,1,0);
-        book = new Item(118,"book","Weapon","",1,3,0.1,0,1,0);
-        chainwhip = new Item(118,"chain whip","Weapon","",1,17,0.2,0,1,0);
-        ropewhip = new Item(118,"rope whip","Weapon","",1,14,0.15,0,1,0);
-        humanleg = new Item(118,"human leg","Weapon","",1,8,0.1,0,1,0);
-        humanarm = new Item(118,"human arm","Weapon","",1,8,0.1,0,1,0);
-        humanskull = new Item(118,"human skull","Weapon","",1,7,0.1,0,1,0);
         
+            //>5dmg
+            book = new Item(118,"book","Weapon","",1,3,0.1,0,1,0);
+            
+            //>10dmg
+            club = new Item(118, "club","Weapon","",1,8,0.1,0,1,0);
+            largestick = new Item(118,"large stick","Weapon","",1,5,0.1,0,1,0);
+            arrow = new Item(118,"arrow","Weapon","",1,7,0.05,0,1,0);
+            brassknuckles = new Item(118,"brass knuckles","Weapon","",1,6,0.05,0,1,0);
+            humanleg = new Item(118,"human leg","Weapon","",1,8,0.1,0,1,0);
+            humanarm = new Item(118,"human arm","Weapon","",1,8,0.1,0,1,0);
+            humanskull = new Item(118,"human skull","Weapon","",1,7,0.1,0,1,0);
+            
+            //>15dmg
+            pitchfork = new Item(1, "pitchfork","Weapon", "",1,10,0.10,0,1,0);
+            baton = new Item(118,"baton","Weapon","",1,12,0.2,0,1,0);
+            mace = new Item(118,"mace","Weapon","",1,14,0.1,0,1,0);
+            ropewhip = new Item(118,"rope whip","Weapon","",1,14,0.15,0,1,0);
+            
+            //>20dmg
+            prisonshank = new Item(118,"prison shank","Weapon","",1,15,0.05,0,1,0);
+            chainwhip = new Item(118,"chain whip","Weapon","",1,17,0.2,0,1,0);
+            
+            //>25dmg
+            sythe = new Item(118,"sythe","Weapon","",1,20,0.2,0,1,0);
+            
+            //>30dmg
+            sword = new Item(1, "sword","Weapon", "",1,25,0.25,0,1,0);
+            axe = new Item(118,"axe","Weapon","",1,27,0.25,0,1,0);
+            
         //armor
 
             //helmets
